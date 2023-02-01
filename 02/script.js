a5e6c5dc113e8e363eb3ef14772cd78853601189
Gabriel Pontel Dal Bó <gabrielpdalbo@gmail.com>
2023-02-01 04:54:47 -0300

    Operações matemáticas

---
+++ 02/script.js
@@ -0,0 +1,9 @@
+/* 
+  Solicite 2 números, faça a soma deles
+  e apresente o resultado final ao usuário
+*/
+
+let numberOne = Number(prompt('Número 1: '))
+let numberTwo = Number(prompt('Número 2: '))
+
+alert(`A soma é: ${numberOne + numberTwo}`)
--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-/* 
-  Solicite 2 números, faça a soma deles
-  e apresente o resultado final ao usuário
+/*
+  Capturar 2 números
+  e fazer as operações matemáticas
+  de soma, subtração, multiplicação,
+  divisão e resto da divisão.
+
+  E para cada operação, mostrar um alerta
+  com o resultado.
 */
 
 let numberOne = Number(prompt('Número 1: '))
 let numberTwo = Number(prompt('Número 2: '))
 
-alert(`A soma é: ${numberOne + numberTwo}`)
+alert(`${numberOne} | ${numberTwo}
+Soma: ${numberOne + numberTwo}
+Subtração: ${numberOne - numberTwo}
+Multiplicação: ${numberOne * numberTwo}
+Divisão: ${numberOne / numberTwo}
+Resto da divisão: ${numberOne % numberTwo}
+`)
