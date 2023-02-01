ded611a177fc280f824560c65525b07d3a191e5a
Gabriel Pontel Dal Bó <gabrielpdalbo@gmail.com>
2023-02-01 04:35:52 -0300

    Somar dois números

---
+++ 01/script.js
@@ -0,0 +1,11 @@
+/*
+  Encontre a solução ao problema:
+
+  Pergunte o nome do usuário e escreva a mensagem:
+  "Olá, [nome do usuário]"
+
+*/
+
+let user = prompt('Informe seu nome:')
+
+alert('Olá, ' + user)
