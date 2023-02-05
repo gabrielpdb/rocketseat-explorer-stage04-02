f456b79b4456cf32cd34807f1d5891d4bbca7c90
Gabriel Pontel Dal Bó <gabrielpdalbo@gmail.com>
2023-02-05 06:59:08 -0300

    Calculando IMC dos pacientes com funções

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 /* 
-  Crie uma lista de pacientes
+  Dada uma lista de pacientes, descubra o IMC de cada paciente e imprima
 
-  Cada paciente dentro da lista, deverá conter
-    nome
-    idade
-    peso
-    altura
+  "Paciente X possui o IMC de: Y"
 
-  Escreva uma lista contendo o nome dos pacientes
+  Onde X é o nome do paciente e Y é o IMC desse paciente
+
+  Crie uma função para fazer o cálculo de IMC
 */
+
+/* peso / (altura * altura) */
 
 const patients = [
   { name: 'Luiz', age: 20, weight: 100, height: 190 },
@@ -16,10 +16,18 @@ const patients = [
   { name: 'Carlos', age: 42, weight: 90, height: 180 }
 ]
 
-let patientsNames = []
-
-for (const patient of patients) {
-  patientsNames.push(patient.name)
+function IMC(weight, height) {
+  return (weight / (height / 100) ** 2).toFixed(2)
 }
 
-alert(patientsNames)
+function printPatientIMC(patient) {
+  return `Paciente ${patient.name} possui o IMC de ${IMC(
+    patient.weight,
+    patient.height
+  )}`
+}
+
+for (const patient of patients) {
+  let IMCmessage = printPatientIMC(patient)
+  alert(IMCmessage)
+}
